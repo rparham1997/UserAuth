0877a197122d6e4d48c2207b4635b1ed4200fc37
philparham <phil.parham@digital-tectonics.com>
2023-09-12 11:06:27 -0400

    Initial Commit

---
+++ SwiftUIAuthentication/SwiftUIAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthenticationApp.swift
+//  SwiftUIAuthentication
+//
+//  Created by Ramar Parham on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
