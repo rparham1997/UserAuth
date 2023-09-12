f9cb68727a3cdcd278500885e839cbe6b4aef673
philparham <phil.parham@digital-tectonics.com>
2023-09-12 19:55:28 -0400

    User Profile, Sign up, and Login set up

--- SwiftUIAuthentication/Core/Auth/LoginView.swift
+++ SwiftUIAuthentication/Core/Auth/LoginView.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // image
+                Image("beach")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+                // form fields
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                //sign in button
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                // sign up button
+                
+                NavigationLink {
+                    RegistrationView()
+                        .navigationBarBackButtonHidden(true)
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Dont have an account?")
+                        Text("Sign up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+            }
+        }
     }
 }
 
